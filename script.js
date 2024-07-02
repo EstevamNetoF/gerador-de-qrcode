8450904082a9fa50ff3ff75aa9908b3c826a569f
estevamnetof <estevam.neto07@gmail.com>
2024-07-02 19:57:56 -0300

    mudança de API (google chart foi desativada)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function GerarQRCode(){
     var inputUsuario = document.querySelector('textarea').value;
-    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chld=H&chl=';
+    var GoogleChartAPI = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${inputUsuario}`;
     var conteudoQRCode = GoogleChartAPI + encodeURIComponent(inputUsuario);
     document.querySelector('#QRCodeImage').src = conteudoQRCode;
 
